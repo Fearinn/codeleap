8bba99c449f7e046cc5a1c265eb28f3f2106fb69
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-04-09 14:18:10 -0300

    refactor: add css variables for colors

---
+++ src/assets/cssVariables.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  brandPrimary: "#7695EC",
+  backgroundPrimary: "#FFFFFF",
+  buttonPositive: "#47B960",
+  buttonNegative: "#FF5151",
+  fontPrimary: "#000000",
+  fontSecondary: "#FFFFFF",
+};
