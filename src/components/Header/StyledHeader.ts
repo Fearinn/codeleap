8379b9003888e8577897239ffc8b4d4d3521ffea
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-04-09 19:32:00 -0300

    refactor(components): create Header

---
+++ src/components/Header/StyledHeader.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { colors } from "../../assets/cssVariables";
+
+export const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  gap: 0.5rem;
+  background-color: ${colors.brandPrimary};
+  color: ${colors.fontSecondary};
+  font-size: 1.375rem;
+  font-weight: bold;
+  padding: 2.312rem 1.685rem;
+  width: 100%;
+`;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import { StyledHeader } from "./StyledHeader";
+import { THeader } from "./THeader";
+
+export function Header({
+  children,
+  editable = false,
+  onDelete,
+  onEdit,
+}: THeader) {
+  return (
+    <StyledHeader>
+      <h1>{children}</h1>
+      {editable && (
+        <div>
+          <button
+            aria-label="delete post"
+            onClick={() => {
+              onDelete && onDelete();
+            }}
+          >
+            <img src="/delete.svg" alt="" />
+          </button>
+          <button
+            aria-label="edit post"
+            onClick={() => {
+              onEdit && onEdit();
+            }}
+          >
+            <img src="/edit.svg" />
+          </button>
+        </div>
+      )}
+    </StyledHeader>
+  );
+}
