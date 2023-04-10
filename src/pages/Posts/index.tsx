f72b8bba618b0584a995a42fb9e1f058f96b270a
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-04-10 01:16:12 -0300

    feat(Posts): add Message and pagination

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,37 +1,75 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { StyledPosts } from "./StyledPosts";
 import { postsService } from "../../actions/services/posts";
 import { Header } from "../../components/Header";
 import { PostCard } from "../../components/PostCard";
-import { NewPost } from "../../components/newPost";
+import { NewPost } from "../../components/NewPost";
 import { useDispatch } from "react-redux";
 import { useTypedSelector } from "../../redux/hooks";
 import { postsActions } from "../../redux/reducers/posts";
+import { StyledButton } from "../../components/Button/StyledButton";
+import { Message } from "../../components/Message";
+import { errorActions } from "../../redux/reducers/error";
 
 export function Posts() {
   const posts = useTypedSelector((state) => state.postsReducer);
   const dispatch = useDispatch();
 
-  useEffect(() => {
+  const [nextPage, setNextPage] = useState<string>();
+  const [previousPage, setPreviousPage] = useState<string>();
+  const error = useTypedSelector((state) => state.errorReducer);
+  const sucess = useTypedSelector((state) => state.successReducer);
+
+  function fetchPosts(path?: string) {
     postsService
-      .fetch()
-      .then((response) => dispatch(postsActions.setPosts(response.results)))
-      .catch(() =>
-        alert("There was an unexpected error! Please try again later.")
-      );
+      .fetchPaginated(path)
+      .then(({ results, next, previous }) => {
+        dispatch(postsActions.setPosts(results));
+        setNextPage(next || undefined);
+        setPreviousPage(previous || undefined);
+      })
+      .catch(() => dispatch(errorActions.setError(true)));
+  }
+
+  useEffect(() => {
+    fetchPosts();
   }, []);
 
   return (
     <>
       <Header>CodeLeap Network</Header>
-      <StyledPosts>
-        <ul className="posts">
-          <NewPost />
-          {posts.map((post) => {
-            return <PostCard {...post} key={post.id}></PostCard>;
-          })}
-        </ul>
-      </StyledPosts>
+      <main id="main">
+        <StyledPosts>
+          {error && <Message error={true} />}
+          {sucess && <Message error={false} />}
+          <ul className="posts">
+            <NewPost />
+            {posts.map((post) => {
+              return <PostCard {...post} key={post.id}></PostCard>;
+            })}
+          </ul>
+          <div className="pagination">
+            <StyledButton
+              disabled={!previousPage}
+              variant="primary"
+              onClick={() => {
+                fetchPosts(previousPage);
+              }}
+            >
+              {" <-- Previous "}
+            </StyledButton>
+            <StyledButton
+              disabled={!nextPage}
+              variant="primary"
+              onClick={() => {
+                fetchPosts(nextPage);
+              }}
+            >
+              {"Next -->"}
+            </StyledButton>
+          </div>
+        </StyledPosts>
+      </main>
     </>
   );
 }
