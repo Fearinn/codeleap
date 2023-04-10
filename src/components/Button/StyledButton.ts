7de5a2b1bee48b2a204798c565fd608999c27e12
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-04-10 01:04:52 -0300

    css(StyledButton): fix positive color with negative variant

--- src/components/Button/StyledButton.ts
+++ src/components/Button/StyledButton.ts
@@ -5,7 +5,7 @@ import { colors } from "../../assets/cssVariables";
 function handleVariant(variant: TButtonVariant) {
   if (variant === "primary") return colors.brandPrimary;
   if (variant === "positive") return colors.buttonPositive;
-  if (variant === "negative") return colors.buttonPositive;
+  if (variant === "negative") return colors.buttonNegative;
   if (variant === "transparent") return "transparent";
 }
 
@@ -16,6 +16,8 @@ export const StyledButton = styled.button`
     props.variant === "transparent"
       ? colors.fontPrimary
       : colors.fontSecondary};
+  ${(props: TButton) =>
+    props.variant === "transparent" && `border: 1px solid ${colors.grayDark};`}
   padding: 0.5rem 2rem;
   width: fit-content;
 `;
