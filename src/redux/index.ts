64ccfcd9dfd651d0bbe372ac76df42c1ef863e8b
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-04-10 01:10:58 -0300

    refactor(redux): add error and success to store

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usernameReducer } from "./reducers/username";
 import { postsReducer } from "./reducers/posts";
+import { errorReducer } from "./reducers/error";
+import { successReducer } from "./reducers/success";
 
 export const store = configureStore({
   reducer: {
     usernameReducer,
     postsReducer,
+    errorReducer,
+    successReducer,
   },
 });
 
