4e948b6428c215c60eee5c9428d435901708d74f
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-04-10 01:06:41 -0300

    refactor(postsServices): add fetchPaginated, edit and delete

--- src/actions/services/posts.ts
+++ src/actions/services/posts.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { instance } from ".";
 import { TPost } from "../../types/TPost";
 import { TResponse } from "../../types/TResponse";
@@ -5,7 +6,10 @@ import { TResponse } from "../../types/TResponse";
 export const postsService = {
   fetch: async () => {
     const response = await instance.get<TResponse>("");
-    console.log(response.data);
+    return response.data;
+  },
+  fetchPaginated: async (path = "https://dev.codeleap.co.uk/careers/") => {
+    const response = await axios.get<TResponse>(path);
     return response.data;
   },
   save: async (username: string, title: string, content: string) => {
@@ -14,6 +18,17 @@ export const postsService = {
       title,
       content,
     });
+    return response.status;
+  },
+  edit: async (id: number, title: string, content: string) => {
+    const response = await instance.patch<TPost>(`${id.toString()}/`, {
+      title,
+      content,
+    });
+    return response.status;
+  },
+  delete: async (id: number) => {
+    const response = await instance.delete<TPost>(`${id.toString()}/`);
     return response.status;
   },
 };
